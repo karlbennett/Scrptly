f84e652c3c83fa5b911c63ec376947825260ed17
Karl Bennett <where_1ts@yahoo.com>
2012-06-05 00:55:49 +0100

    - Created a MissingRequestParameterError that is used to indicate when a request is missing required request parameters.  - Updated the RequestHandlerResolver so that it now checks to make sure a request contains all it's require request parameters.  - Updated the RequestHandlerResolverTests so that they now test handlers with request parameters.

--- test/requests/RequestHandlerResolverTest.js
+++ test/requests/RequestHandlerResolverTest.js
@@ -22,6 +22,15 @@ var GET = MockRequest.GET;
 var POST = MockRequest.POST;
 var TEST_URL = MockRequest.TEST_URL;
 var TEST_PATH = MockRequest.TEST_PATH;
+var TEST_PARAMETER_ONE_NAME = MockRequest.TEST_PARAMETER_ONE_NAME;
+var TEST_PARAMETER_ONE_VALUE = MockRequest.TEST_PARAMETER_ONE_VALUE;
+var TEST_PARAMETER_ONE = MockRequest.TEST_PARAMETER_ONE;
+var TEST_PARAMETER_TWO_NAME = MockRequest.TEST_PARAMETER_TWO_NAME;
+var TEST_PARAMETER_TWO_VALUE = MockRequest.TEST_PARAMETER_TWO_VALUE;
+var TEST_PARAMETER_TWO = MockRequest.TEST_PARAMETER_TWO;
+var TEST_PARAMETER_THREE_NAME = MockRequest.TEST_PARAMETER_THREE_NAME;
+var TEST_PARAMETER_THREE_VALUE = MockRequest.TEST_PARAMETER_THREE_VALUE;
+var TEST_PARAMETER_THREE = MockRequest.TEST_PARAMETER_THREE;
 
 var MESSAGE_404 = ErrorHandlerResolver.MESSAGE_404;
 var MESSAGE_500 = ErrorHandlerResolver.MESSAGE_500;
@@ -70,14 +79,14 @@ var tests = {
         var request = MockRequest(GET, TEST_URL);
 
         expects(
-            function () {
+        function () {
             
-                RequestHandlerResolver({})(request, null)
+            RequestHandlerResolver({})(request, null)
             
-            }, 
-            isPageNotFoundError, 
-            'error should be thrown if no handlers supplied'
-            );
+        }, 
+        isPageNotFoundError, 
+        'error should be thrown if no handlers supplied'
+    );
     },
     
     'testRequestHandlerCallbackWithNullHandlers': function () {
@@ -85,14 +94,14 @@ var tests = {
         var request = MockRequest(GET, TEST_URL);
 
         expects(
-            function () {
+        function () {
             
-                RequestHandlerResolver(null)(request, null)
+            RequestHandlerResolver(null)(request, null)
             
-            }, 
-            isPageNotFoundError, 
-            'error should be thrown if no handlers supplied'
-            );
+        }, 
+        isPageNotFoundError, 
+        'error should be thrown if no handlers supplied'
+    );
     },
     
     'testRequestHandlerCallbackWithUndefinedHandlers': function () {
@@ -100,14 +109,14 @@ var tests = {
         var request = MockRequest(GET, TEST_URL);
 
         expects(
-            function () {
+        function () {
             
-                RequestHandlerResolver()(request, null)
+            RequestHandlerResolver()(request, null)
             
-            }, 
-            isPageNotFoundError, 
-            'error should be thrown if no handlers supplied'
-            );
+        }, 
+        isPageNotFoundError, 
+        'error should be thrown if no handlers supplied'
+    );
     },
     
     'testRequestHandlerWithCustomHandlerCallback': function () {
@@ -118,8 +127,39 @@ var tests = {
         var called = false;
 
         var handlers = {};
-        handlers[TEST_PATH + ':' + request.method] = function () { 
-            called = true; 
+        handlers[TEST_PATH + ':' + request.method] = {
+            'function': function () { 
+                called = true; 
+            }
+        };
+
+        RequestHandlerResolver(handlers)(request, response);
+        
+        ok(!response.writeHeadCalled, 'writeHead() should not be called.');
+        ok(!response.writeCalled, 'write() should not be called.');
+        ok(!response.endCalled, 'end() should not be called.');
+        
+        ok(called, 'custom handler should be called');
+    },
+    
+    'testRequestHandlerWithCustomHandlerCallbackAndParameters': function () {
+
+        var request = MockRequest(GET, TEST_URL + '?' + TEST_PARAMETER_ONE 
+            + '&' + TEST_PARAMETER_TWO + '&' + TEST_PARAMETER_THREE);
+        var response = MockResponse();
+
+        var called = false;
+
+        var handlers = {};
+        handlers[TEST_PATH + ':' + request.method] = {
+            'function': function (request, respoonse, one, two, three) { 
+                called = true;
+                
+                equal(one, TEST_PARAMETER_ONE_VALUE, 'argument one value should be correct');
+                equal(two, TEST_PARAMETER_TWO_VALUE, 'argument two value should be correct');
+                equal(three, TEST_PARAMETER_THREE_VALUE, 'argument three value should be correct');
+            },
+            'parameters': ['one', 'two', 'three']
         };
 
         RequestHandlerResolver(handlers)(request, response);
