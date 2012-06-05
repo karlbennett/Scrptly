d12a41af5fde72aa3f4d7ea56000c078c39056a9
Karl Bennett <where_1ts@yahoo.com>
2012-06-05 12:19:51 +0100

    Created tests for the new extractParameterValues and findMissingParameterNames functions.

--- test/utils/NodeUtilsTest.js
+++ test/utils/NodeUtilsTest.js
@@ -17,9 +17,102 @@ var TEST_HEADERS = MockResponse.TEST_HEADERS;
 var CONTENT_TYPE = MockResponse.CONTENT_TYPE;
 var TEST_BODY = MockResponse.TEST_BODY;
 
+var TEST_PARAMETERS = { 'one': 1, 'two': 2, 'three': 3 };
+var TEST_PARAMETER_NAMES_SUBSET = [ 'one', 'three' ];
+var TEST_PARAMETER_VALUES_SUBSET = [ 1, 3 ];
+var TEST_PARAMETER_NAMES = [ 'one', 'two', 'three' ];
+var TEST_PARAMETER_VALUES = [ 1, 2, 3 ];
+var TEST_PARAMETER_NAMES_SUPERSET = [ 'one', 'two', 'three', 'four' ];
+var TEST_PARAMETER_VALUES_SUPERSET = [ 1, 2, 3, 4 ];
+var TEST_PARAMETER_MISSING_NAMES = [ 'four' ];
+
 
 var tests = {
 
+    'testExtractParameterValues': function () {
+
+        var values = nodeUtils.extractParameterValues(TEST_PARAMETERS, TEST_PARAMETER_NAMES);
+        
+        deepEqual(values, TEST_PARAMETER_VALUES, 'the correct values are extracted')
+    },
+    
+    'testExtractParameterValuesWithSubset': function () {
+
+        var values = nodeUtils.extractParameterValues(TEST_PARAMETERS, TEST_PARAMETER_NAMES_SUBSET);
+        
+        deepEqual(values, TEST_PARAMETER_VALUES_SUBSET, 'the correct values are extracted')
+    },
+    
+    'testExtractParameterValuesWithSuperset': function () {
+
+        var values = nodeUtils.extractParameterValues(TEST_PARAMETERS, TEST_PARAMETER_NAMES_SUPERSET);
+        
+        deepEqual(values, TEST_PARAMETER_VALUES, 'the correct values are extracted')
+    },
+    
+    'testExtractParameterValuesWithNullParameters': function () {
+
+        var values = nodeUtils.extractParameterValues(null, TEST_PARAMETER_NAMES_SUPERSET);
+        
+        deepEqual(values, [], 'no values should be extracted')
+    },
+    
+    'testExtractParameterValuesWithNullParameterNames': function () {
+
+        var values = nodeUtils.extractParameterValues(TEST_PARAMETERS, null);
+        
+        deepEqual(values, [], 'no values should be extracted')
+    },
+    
+    'testExtractParameterValuesWithUndefinedParameterNames': function () {
+
+        var values = nodeUtils.extractParameterValues(TEST_PARAMETERS);
+        
+        deepEqual(values, [], 'no values should be extracted')
+    },
+    
+    'testFindMissingParameterNames': function () {
+
+        var values = nodeUtils.findMissingParameterNames(TEST_PARAMETERS, TEST_PARAMETER_NAMES);
+        
+        deepEqual(values, [], 'no missing parameter names should be found')
+    },
+    
+    'testFindMissingParameterNamesWithSubset': function () {
+
+        var values = nodeUtils.findMissingParameterNames(TEST_PARAMETERS, TEST_PARAMETER_NAMES_SUBSET);
+        
+        deepEqual(values, [], 'no missing parameter names should be found')
+    },
+    
+    'testFindMissingParameterNamesWithSuperset': function () {
+
+        var values = nodeUtils.findMissingParameterNames(TEST_PARAMETERS, TEST_PARAMETER_NAMES_SUPERSET);
+        
+        deepEqual(values, TEST_PARAMETER_MISSING_NAMES, 'missing parameter names should be found')
+    },
+    
+    'testFindMissingParameterNamesWithNullParameters': function () {
+
+        var values = nodeUtils.findMissingParameterNames(null, TEST_PARAMETER_NAMES);
+        
+        deepEqual(values, TEST_PARAMETER_NAMES, 'missing parameter names should be found')
+    },
+    
+    'testFindMissingParameterNamesWithNullParameterNames': function () {
+
+        var values = nodeUtils.findMissingParameterNames(TEST_PARAMETERS, null);
+        
+        deepEqual(values, [], 'no values should be extracted')
+    },
+    
+    'testFindMissingParameterNamesWithUndefinedParameterNames': function () {
+
+        var values = nodeUtils.findMissingParameterNames(TEST_PARAMETERS);
+        
+        deepEqual(values, [], 'no values should be extracted')
+    },
+    
     'testWriteResponse': function () {
 
         var response = MockResponse(TEST_CODE, TEST_HEADERS, TEST_BODY);
